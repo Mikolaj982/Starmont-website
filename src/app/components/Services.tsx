130b47775d1c05817375cfabef33c5e0a4b93810
Mikolaj982 <107889765+Mikolaj982@users.noreply.github.com>
2026-02-13 15:25:05 +0100

    UI/UX change

--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -49,9 +49,8 @@ const Services = () => {
             <div className="flex-1 overflow-y-auto">
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
                     {services.map((service, i) => (
-                        <div>
+                        <div key={i}>
                             <div
-                                key={i}
                                 className="bg-white/10 backdrop-blur rounded-lg p-2 flex items-center lg:p-4 flex-row justify-between"
                             >
                                 <div>
