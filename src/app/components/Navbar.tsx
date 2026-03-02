bea68505e8cf1719f2fdb84e5da5b9671e394390
Mikolaj982 <107889765+Mikolaj982@users.noreply.github.com>
2026-03-02 17:51:11 +0100

    change logo

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,6 @@
 import { Bars3Icon } from '@heroicons/react/20/solid'
 import React, { useState } from 'react'
 import Image from 'next/image';
-import logo from '../../../public/images/favicon.png';
 import NavigationMenu from './NavigationMenu';
 
 const Navbar: React.FC = () => {
@@ -13,7 +12,7 @@ const Navbar: React.FC = () => {
 
     return (<>
         <div className='fixed top-0 w-[100%] flex justify-between z-10'>
-            <Image src={logo} alt='starmont logo' className='relative -top-7 -left-3 m-6 w-24 lg:w-32 lg:-top-10' />
+            <Image src={'/images/favicon.ico'} priority alt='starmont logo' className='relative -top-7 -left-3 m-6 w-24 lg:w-32 lg:-top-10' />
             <Bars3Icon className='h-[25px] text-white m-6 lg:hidden' onClick={() => toggleSidebar()} />
             <NavigationMenu setIsOpen={setIsOpen} isOpen={isOpen} />
         </div>
